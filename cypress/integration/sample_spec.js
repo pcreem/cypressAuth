5b495f9fa1ea30f98ce401cfd01b4f95fc1698eb
pcreem <creempepper@gmail.com>
2020-10-11 19:41:47 +0800

    local version

--- cypress/integration/sample_spec.js
+++ cypress/integration/sample_spec.js
@@ -5,41 +5,38 @@ describe('Login Test', () => {
 
     it('Visits the test page', () => {
       cy.visit('http://localhost:3000')
-      .then(()=>{
-        cy.contains('Sign in').click()
-        cy.url().should('include', '/api/auth/signin')
+      cy.contains('Sign in').click()
+      cy.url().should('include', '/api/auth/signin')
 
-        cy.get('#input-email-for-email-provider')
-          .type(randomEmail)
-          .should('have.value', randomEmail)
+      cy.get('#input-email-for-email-provider')
+        .type(randomEmail)
+        .should('have.value', randomEmail)
     
         cy.contains('Sign in with Email').click()
-        cy.contains('Check your email')
-      })
-      
+        cy.contains('Check your email')      
         
     });
 
-    // it('should send an email containing a verification link', () => {
-    //     const inboxUrl = Cypress.env('inboxUrl')
-    //     const token  = Cypress.env('api_token')
+    it('should send an email containing a verification link', () => {
+        const inboxUrl = Cypress.env('inboxUrl')
+        const token  = Cypress.env('api_token')
 
-    //     cy.getLastEmail().then(html => {
+        cy.getLastEmail().then(html => {
      
-    //       const link = html.match(/href="([^"]*)/)[1]
-    //       cy.expect(link).to.contains('/api/auth/callback/email')
-    //       cy.visit(link);
-    //       cy.contains(`Signed in as ${randomEmail}`)
-    //       cy.reload()
-    //       cy.contains(`Signed in as ${randomEmail}`)
-    //       cy.request({
-    //         method: 'PATCH',
-    //         url: `${inboxUrl}/clean`,
-    //         headers: {
-    //             'Api-Token': token,
-    //             }
-    //         });
-    //     });
-    //   });
+          const link = html.match(/href="([^"]*)/)[1]
+          cy.expect(link).to.contains('/api/auth/callback/email')
+          cy.visit(link);
+          cy.contains(`Signed in as ${randomEmail}`)
+          cy.reload()
+          cy.contains(`Signed in as ${randomEmail}`)
+          cy.request({
+            method: 'PATCH',
+            url: `${inboxUrl}/clean`,
+            headers: {
+                'Api-Token': token,
+                }
+            });
+        });
+      });
   })
 
